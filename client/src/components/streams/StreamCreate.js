c57297706131d7562024f97c3288fe2a456fd428
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-02-28 13:37:27 +0530

    Added Server :card_index_dividers:

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Field, reduxForm, updateSyncErrors } from "redux-form";
+import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -23,9 +25,9 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     // console.log(this.props);
@@ -66,7 +68,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
-  validate: validate,
+  validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
