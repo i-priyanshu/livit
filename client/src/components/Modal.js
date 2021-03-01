7f50685c0f6172c4818f39b545ea062cce171560
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-03-02 01:26:45 +0530

    UI : Added Delete operation

---
+++ client/src/components/Modal.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Modal = (props) => {
+  return ReactDOM.createPortal(
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active ">
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui basic modal visible active inverted"
+      >
+        <div className="ui icon header">
+          <i class="trash icon"></i>
+          {props.title}
+        </div>
+        <div className="content ui segment">{props.content}</div>
+        <div className="actions">{props.actions}</div>
+      </div>
+    </div>,
+    document.querySelector("#modal")
+  );
+};
+
+export default Modal;
